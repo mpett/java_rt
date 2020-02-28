6ed91ed4baa751340f7b4db52417fa88fa061ba8
Martin Pettersson <kontakt@martinpettersson.se>
2020-02-28 19:19:58 +0100

    Euler update, even fib sum.

--- Euler.java
+++ Euler.java
@@ -4,7 +4,42 @@ import java.io.InputStreamReader;
 
 public class Euler {
     public static void main(String[] args) throws IOException {
-        multiples();
+        //multiples();
+        int fibonacciSum = evenFibonacciSum(4000000);
+        System.out.println(fibonacciSum);
+    }
+
+    public static int evenFibonacciSum(int limit) {
+        int sum = 0;
+        int fibonacciNumber = 0;
+        int n = 0;
+
+        while (fibonacciNumber <= limit) {
+            fibonacciNumber = fibonacci(n);
+            if (fibonacciNumber <= limit && fibonacciNumber % 2 == 0) {
+                sum += fibonacciNumber;
+            }
+            n++;
+        }
+        
+        return sum;
+    }
+
+    public static void printFibonacciSequence(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
     }
 
     public static void multiples() throws IOException {
