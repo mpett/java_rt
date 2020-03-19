6b92a47d7fe6c5ab19960549ad1680ba737ad980
Martin Pettersson <kontakt@martinpettersson.se>
2020-03-19 03:19:29 +0100

    cpp: more operators

--- cpp_rt/main.cpp
+++ cpp_rt/main.cpp
@@ -76,13 +76,23 @@ class vec3 {
         return os;
     }
 
-    inline void make_unit_vector();
-
-    inline void vec3::make_unit_vector() {
+    inline void make_unit_vector() {
         float k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
         e[0] *= k;
         e[1] *= k;
         e[2] *= k;
+    }
+
+    friend inline vec3 operator+(const vec3& v1, const vec3& v2) {
+        return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+    }
+
+    friend inline vec3 operator-(const vec3& v1, const vec3& v2) {
+        return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
+    }
+
+    friend inline vec3 operator*(const vec3& v1, const vec3& v2) {
+        return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
     }
 
     float e[3];
