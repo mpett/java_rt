9d7e7d8ef15b046814474f140b8527d1dde0164a
Martin Pettersson <kontakt@martinpettersson.se>
2020-03-20 15:04:09 +0100

    Cpp: added first red circle

--- cpp_rt/vec3.h
+++ cpp_rt/vec3.h
@@ -2,8 +2,6 @@
 #include <math.h>
 #include <stdlib.h>
 
-// Begin Vector
-
 class vec3 {
     public:
         vec3() {}
@@ -117,7 +115,7 @@ inline vec3 operator/(vec3 v, float t) {
     return vec3(v.e[0] / t, v.e[1] / t, v.e[2] / t);
 }
 
-inline float dot(const vec3& v1, const vec3& v2) {
+inline float dot(vec3& v1, vec3& v2) {
     return v1.e[0] * v2.e[0] + 
             v1.e[1] * v2.e[1] +
             v1.e[2] * v2.e[2];
@@ -174,79 +172,4 @@ inline vec3& vec3::operator/=(const float t) {
 
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
-}
-
-// Begin Ray
-
-class ray {
-    public:
-        ray() {}
-        
-        ray(const vec3& a, const vec3& b) {
-            A = a; 
-            B = b;
-        }
-
-        vec3 origin() const {
-            return A;
-        }
-
-        vec3 direction() const {
-            return B;
-        }
-
-        vec3 point_at_parameter(float t) const {
-            return A + t*B;
-        }
-
-        vec3 A;
-        vec3 B;
-};
-
-// Begin Main
-
-bool hit_sphere(const vec3& center, float radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    float a = dot(r.direction(), r.direction());
-    float b = 2.0 * dot(oc, r.direction());
-    float c = dot(oc, oc) - radius * radius;
-    float discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
-}
-
-vec3 color(const ray& r) {
-    if (hit_sphere(vec3(0, 0, -1), 0.5, r)) {
-        return vec3(1, 0, 0);
-    }
-    vec3 unit_direction = unit_vector(r.direction());
-    float t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
-}
-
-int main()
-{
-    int nx = 200;
-    int ny = 100;
-
-    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
-
-    for (int j = ny-1; j >= 0; j--) {
-        for (int i = 0; i < nx; i++) {
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-            vec3 col = color(r);
-            int ir = int(255.99 * col[0]);
-            int ig = int(255.99 * col[1]);
-            int ib = int(255.99 * col[2]);
-            std::cout << ir << " " << ig << " " << ib << "\n";
-        }
-    }
-    
-    return 0;
 }
