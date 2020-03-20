4aa15d51f9fb3895bfed8e21da900da538c045c5
Martin Pettersson <kontakt@martinpettersson.se>
2020-03-21 00:38:48 +0100

    Hit function for sphere class

--- cpp_rt/main.cpp
+++ cpp_rt/main.cpp
@@ -229,6 +229,34 @@ class sphere : public hittable {
         float radius;
 };
 
+bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b*b - a*c;
+
+    if (discriminant > 0) {
+        float temp = (-b - sqrt(discriminant)) / a;
+        if (temp < t_max && temp > t_min) {
+            rec.t = temp;
+            rec.p = r.point_at_parameter(rec.t);
+            rec.normal = (rec.p - center) / radius;
+            return true;
+        }
+
+        temp = (-b + sqrt(discriminant)) / a;
+
+        if (temp < t_max && temp > t_min) {
+            rec.t = temp;
+            rec.p = r.point_at_parameter(rec.t);
+            rec.normal = (rec.p - center) / radius;
+            return true;
+        }
+    }
+    return false;
+}
+
 // Begin Main
 
 float hit_sphere(const vec3& center, float radius, const ray& r) {
