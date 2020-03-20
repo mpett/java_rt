4c647e5b410bac30e527d6a5b4badaf9722f73f8
Martin Pettersson <kontakt@martinpettersson.se>
2020-03-20 14:14:52 +0100

    Cpp started on ray class

--- cpp_rt/main.cpp
+++ cpp_rt/main.cpp
@@ -174,6 +174,31 @@ inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
 
+class ray {
+    public:
+        ray() {}
+        
+        ray(const vec3& a, const vec3& b) {
+            A = a; 
+            B = b;
+        }
+
+        vec3 origin() const {
+            return A;
+        }
+
+        vec3 direction() const {
+            return B;
+        }
+
+        vec3 point_at_parameter(float t) const {
+            return A + t*B;
+        }
+
+        vec3 A;
+        vec3 B;
+};
+
 int main()
 {
     int nx = 200;
