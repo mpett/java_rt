7fcaed90835712712bf29d03cd4cb6ba2df452a4
Martin Pettersson <kontakt@martinpettersson.se>
2020-03-22 21:22:42 +0100

    Functions for generating random double values.

--- cpp_rt/main.cpp
+++ cpp_rt/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <math.h>
 #include <stdlib.h>
+#include <functional>
+#include <random>
+#include <cstdlib>
 
 #include "float.h"
 
@@ -290,6 +293,20 @@ bool hittable_list::hit(const ray& r, float t_min, float t_max, hit_record& rec)
     }
     
     return hit_anything;
+}
+
+// Begin Random Double
+
+inline double random_double_cstd() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    static std::function<double()> rand_generator =
+        std::bind(distribution, generator);
+    return rand_generator();
 }
 
 // Begin Main
