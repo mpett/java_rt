c587722899715f4010214653a86b9281c674e7b7
Martin Pettersson <kontakt@martinpettersson.se>
2020-03-25 19:47:18 +0100

    vec3 class, version 3.0

--- 3.0_cpp_rt/main.cpp
+++ 3.0_cpp_rt/main.cpp
@@ -1,13 +1,81 @@
 #include <iostream>
+#include <math.h>
+
+class vec3 {
+    public:
+        vec3() : e{0, 0, 0} {}
+
+        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+        double x() const {
+            return e[0];
+        }
+
+        double y() const {
+            return e[1];
+        }
+
+        double z() const {
+            return e[2];
+        }
+
+        vec3 operator-() const {
+            return vec3(-e[0], -e[1], -e[2]);
+        }
+
+        double operator[](int i) const {
+            return e[i];
+        }
+
+        double& operator[](int i) {
+            return e[i];
+        }
+
+        vec3& operator+=(const vec3& v) {
+            e[0] += v.e[0];
+            e[1] += v.e[1];
+            e[2] += v.e[2];
+            return *this;
+        }
+
+        vec3& operator*=(const double t) {
+            e[0] *= t;
+            e[1] *= t;
+            e[2] *= t;
+            return *this;
+        }
+
+        vec3& operator/=(const double t) {
+            return *this *= 1 / t;
+        }
+
+        double length() const {
+            return sqrt(length_squared());
+        }
+
+        double length_squared() const {
+            return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+        }
+
+        void write_color(std::ostream out) {
+            out << static_cast<int>(255.99 * e[0]) << " "
+            << static_cast<int>(255.99 * e[1]) << " "
+            << static_cast<int>(255.99 * e[2]) << "\n";
+        }
+
+    public:
+        double e[3];
+};
 
 int main() {
     const int image_width = 200;
     const int image_height = 100;
 
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+    std::cerr << "Scanlines remaining: ";
 
     for (int j = image_height - 1; j >= 0; --j) {
-        std::cerr << "Scanlines remaining: " << j << " " << std::flush;
+        std::cerr << j << " " << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto r = double(i) / image_width;
             auto g = double(j) / image_height;
