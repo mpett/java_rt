0f7a05a8052add8a80577b28bf36310fdb250dac
Martin Pettersson <kontakt@martinpettersson.se>
2020-03-31 10:13:12 +0200

    Constants, headers, utility functions

--- 3.0_cpp_rt/main.cpp
+++ 3.0_cpp_rt/main.cpp
@@ -2,6 +2,33 @@
 #include <math.h>
 #include <memory>
 #include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+
+// Usings
+
+using std::shared_ptr;
+using std::make_shared;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180;
+}
+
+inline double ffmin(double a, double b) {
+    return a <= b ? a : b;
+}
+
+inline double ffmax(double a, double b) {
+    return a >= b ? a : b;
+}
 
 // Begin Vector
 
