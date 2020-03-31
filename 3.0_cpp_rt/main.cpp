b3470dbd9c3f797deb73f8bd0c73c92135533d93
Martin Pettersson <kontakt@martinpettersson.se>
2020-03-31 09:49:03 +0200

    Hittable list

--- 3.0_cpp_rt/main.cpp
+++ 3.0_cpp_rt/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <math.h>
+#include <memory>
+#include <vector>
 
 // Begin Vector
 
@@ -201,6 +203,41 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
     }
     
     return false;
+}
+
+// Hittable List
+
+class hittable_list : public hittable {
+    public:
+        hittable_list() {}
+
+        hittable_list(std::shared_ptr<hittable> object) { add(object); }
+
+        void add(std::shared_ptr<hittable> object) {
+            objects.push_back(object);
+        }   
+
+        virtual bool hit(const ray& r, double tmin, double tmax, hit_record& rec) const;
+
+    public:
+        std::vector<std::shared_ptr<hittable>> objects;
+
+};
+
+bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+    hit_record temp_rec;
+    bool hit_anything = false;
+    auto closest_so_far = t_max;
+
+    for (const auto& object : objects) {
+        if (object -> hit(r, t_min, closest_so_far, temp_rec)) {
+            hit_anything = true;
+            closest_so_far = temp_rec.t;
+            rec = temp_rec;
+        }
+    }
+    
+    return hit_anything;
 }
 
 // Begin Main
