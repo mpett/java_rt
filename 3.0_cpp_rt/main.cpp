b94b03a4d39045b19af0130ce5fd77eb04c949ab
Martin Pettersson <kontakt@martinpettersson.se>
2020-04-02 19:39:00 +0200

    Glass with proper reflections

--- 3.0_cpp_rt/main.cpp
+++ 3.0_cpp_rt/main.cpp
@@ -394,6 +394,12 @@ class material {
         ) const = 0;
 };
 
+double schlick(double cosine, double ref_idx) {
+    auto r0 = (1 - ref_idx) / (1 + ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * pow((1 - cosine), 5);
+}
+
 // Lambertian
 
 class lambertian : public material {
@@ -442,16 +448,27 @@ class dielectric : public material {
         virtual bool scatter(const ray& r_in, const hit_record& rec, 
                 vec3& attenuation, ray& scattered) const {
             attenuation = vec3(1.0, 1.0, 1.0);
-            double etai_over_etat;
-            if (rec.front_face) {
-                etai_over_etat = 1.0 / ref_idx;
-            } else {
-                etai_over_etat = ref_idx;
+            double etai_over_etat = (rec.front_face) ? (1.0 / ref_idx) : (ref_idx);
+            vec3 unit_direction = unit_vector(r_in.direction());
+            double cos_theta = ffmin(dot(-unit_direction, rec.normal), 1.0);
+            double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+            if (etai_over_etat * sin_theta > 1.0) {
+                vec3 reflected = reflect(unit_direction, rec.normal);
+                scattered = ray(rec.p, reflected);
+                return true;
             }
 
-            vec3 unit_direction = unit_vector(r_in.direction());
+            double reflect_prob = schlick(cos_theta, etai_over_etat);
+
+            if (random_double() < reflect_prob) {
+                vec3 reflected = reflect(unit_direction, rec.normal);
+                scattered = ray(rec.p, reflected);
+            }
+
             vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
             scattered = ray(rec.p, refracted);
+
             return true;
         }
 
@@ -504,10 +521,11 @@ int main() {
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
     hittable_list world;
-    world.add(make_shared<sphere>(vec3(0.0, 0.0, -1.0), 0.5, make_shared<lambertian>(vec3(0.7, 0.3, 0.3))));
+    world.add(make_shared<sphere>(vec3(0.0, 0.0, -1.0), 0.5, make_shared<lambertian>(vec3(0.1, 0.2, 0.5))));
     world.add(make_shared<sphere>(vec3(0.0, -100.5, -1.0), 100, make_shared<lambertian> (vec3(0.8, 0.8, 0.0))));
     world.add(make_shared<sphere>(vec3(1.0, 0.0, -1.0), 0.5, make_shared<metal> (vec3(0.8, 0.6, 0.2), 0.3)));
-    world.add(make_shared<sphere>(vec3(-1.0, 0.0, -1.0), 0.5, make_shared<metal> (vec3(0.8, 0.8, 0.8), 0.1)));
+    world.add(make_shared<sphere>(vec3(-1.0, 0.0, -1.0), 0.5, make_shared<dielectric>(1.5)));
+    world.add(make_shared<sphere>(vec3(-1.0, 0.0, -1.0), -0.45, make_shared<dielectric>(1.5)));
 
     camera cam;
 
