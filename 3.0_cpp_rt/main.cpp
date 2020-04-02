d4503b77a518aff1689c164cb9eb3c2121030c2b
Martin Pettersson <kontakt@martinpettersson.se>
2020-04-02 09:36:28 +0200

    Refactored.

--- 3.0_cpp_rt/main.cpp
+++ 3.0_cpp_rt/main.cpp
@@ -440,20 +440,20 @@ class dielectric : public material {
         dielectric(double ri) : ref_idx(ri) {}
 
         virtual bool scatter(const ray& r_in, const hit_record& rec, 
-            vec3& attenuation, ray& scattered) const {
-                attenuation = vec3(1.0, 1.0, 1.0);
-                double etai_over_etat;
-                if (rec.front_face) {
-                    etai_over_etat = 1.0 / ref_idx;
-                } else {
-                    etai_over_etat = ref_idx;
-                }
-
-                vec3 unit_direction = unit_vector(r_in.direction());
-                vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
-                scattered = ray(rec.p, refracted);
-                return true;
+                vec3& attenuation, ray& scattered) const {
+            attenuation = vec3(1.0, 1.0, 1.0);
+            double etai_over_etat;
+            if (rec.front_face) {
+                etai_over_etat = 1.0 / ref_idx;
+            } else {
+                etai_over_etat = ref_idx;
             }
+
+            vec3 unit_direction = unit_vector(r_in.direction());
+            vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
+            scattered = ray(rec.p, refracted);
+            return true;
+        }
 
         double ref_idx;
 };
